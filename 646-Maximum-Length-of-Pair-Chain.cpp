8f605efc35254fb8d640f273fa733d8f5ea58666
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2024-09-30 01:52:06 +0530

    [646] [Time Beats: 50.30%] [Memory Beats: 51.93%] - LeetPush

--- 646-Maximum-Length-of-Pair-Chain.cpp
+++ 646-Maximum-Length-of-Pair-Chain.cpp
@@ -1,35 +1,20 @@
 class Solution {
-    int dp[2501][2501];
-    int fun(int i,int pre,vector<vector<int>>&a,int n){
-        if(i==n){
-            return 0;
-        }
-        //pick
-        if(pre!=-1 && dp[i][pre]!=-1){
-            return dp[i][pre];
-        }
-        int pick=0;
-        if(pre==-1 || a[i][0]>a[pre][1]){
-            pick=1+fun(i+1,i,a,n);
-        }
-        //not pick
-        int notpick=fun(i+1,pre,a,n);
-        if(pre!=-1){
-            return dp[i][pre]=max(pick,notpick);
-        }
-        return max(pick,notpick);
-    }
-
 
 public:
     int findLongestChain(vector<vector<int>>& a) {
         int n=a.size();
-        memset(dp,-1,sizeof(dp));
         sort(a.begin(),a.end());
-        //recursion pick and not pick
-        //to form the longest lengthed chain possible
-
-        return fun(0,-1,a,n);
-
+        //tabulation
+        vector<int>v(n,1);
+        int maxi=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(a[i][0]>a[j][1]){
+                    v[i]=max(v[i],v[j]+1);
+                    maxi=max(maxi,v[i]);
+                }
+            }
+        }
+        return maxi;
     }
 };
