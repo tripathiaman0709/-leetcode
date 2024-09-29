6e5139c98c8d254b55e39dd6d40d56b669e40320
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2024-09-30 00:55:16 +0530

    [300] [Time Beats: 53.70%] [Memory Beats: 52.76%] - LeetPush

--- 300-Longest-Increasing-Subsequence.cpp
+++ 300-Longest-Increasing-Subsequence.cpp
@@ -1,35 +1,17 @@
 class Solution {
 public:
-    int dp[2501][2501];
-
-    int fun(int i,int pre,vector<int>&a,int n){
-        if(i>=n){
-            return 0;
-        }
-
-        if(pre!=-1 && dp[i][pre]!=-1){
-            return dp[i][pre];
-        }
-        //take
-        //one size increases
-        int take=0;
-        if(pre==-1 || a[i]>a[pre]){
-            take=1+fun(i+1,i,a,n);
-        }
-        int nottake=fun(i+1,pre,a,n);
-        if(pre!=-1){
-            return dp[i][pre]= max(take,nottake);
-        }
-        return max(take,nottake);
-    }
-
     int lengthOfLIS(vector<int>& a) {
         int n=a.size();
-        memset(dp,-1,sizeof(dp));
-        //LIS
-        //simply doing recursion by take and not take then doing memoization
-        //and finally tabuation
-
-        return fun(0,-1,a,n);
+        vector<int>v(n,1);
+        int maxi=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(a[j]<a[i]){
+                    v[i]=max(v[i],v[j]+1);
+                    maxi=max(maxi,v[i]);
+                }
+            }
+        }
+        return maxi;
     }
 };
