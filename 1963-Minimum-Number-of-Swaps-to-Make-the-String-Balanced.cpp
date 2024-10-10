c0b2e9ae027ca1d7b8fec5bd8d81fe26362aa25a
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2024-10-10 14:17:53 +0530

    [1963] [Time Beats: 76.80%] [Memory Beats: 87.29%] - LeetPush

--- 1963-Minimum-Number-of-Swaps-to-Make-the-String-Balanced.cpp
+++ 1963-Minimum-Number-of-Swaps-to-Make-the-String-Balanced.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
     int minSwaps(string s) {
-        int n=s.size();
-        stack<char>st;
-        int count=0;
-        for(int i=0;i<n;i++){
-            if(s[i]==']'){
-                if(st.empty()){
-                    count++;
-                }else{
-                    st.pop();
-                }
-            }else{
-                st.push('[');
-            }
+        int ans = 0, count = 0;
+        for(char c : s){
+            if (c == '[') count++;
+            else if(c == ']' and count > 0) count --;
         }
-        int ans=(count+1)/2;
+        ans = (count+1)/2;
         return ans;
     }
 };
