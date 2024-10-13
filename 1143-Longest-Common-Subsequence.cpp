995a436ae1cfa7bf9f2854dad0de36d13200f0fc
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2024-10-14 05:04:14 +0530

    [1143] [Time Beats: 92.96%] [Memory Beats: 84.77%] - LeetPush

--- 1143-Longest-Common-Subsequence.cpp
+++ 1143-Longest-Common-Subsequence.cpp
@@ -5,7 +5,7 @@ public:
         int dp[n+1][m+1];
         for(int i=0;i<=n;i++){
             for(int j=0;j<=m;j++){
-                if(i==0 || j==0) dp[i][j]=0;
+                dp[i][j]=0;
             }
         }
         for(int i=1;i<=n;i++){
