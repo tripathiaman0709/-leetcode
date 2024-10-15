b0534c1e8ba0905e294873e91bdc21cc15f4b56e
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2024-10-15 21:51:52 +0530

    [1143] [Time Beats: 33.31%] [Memory Beats: 79.78%] - LeetPush

--- 1143-Longest-Common-Subsequence.cpp
+++ 1143-Longest-Common-Subsequence.cpp
@@ -1,21 +1,27 @@
 class Solution {
+
+int dp[1001][1001];
+
+int fun(int i,int j,string &s1,string &s2,int n,int m){
+    if(i>=n || j>=m){
+        return 0;
+    }
+    if(dp[i][j]!=-1){
+        return dp[i][j];
+    }
+    if(s1[i]==s2[j]){
+        return dp[i][j]=1+fun(i+1,j+1,s1,s2,n,m);
+    }else{
+        return dp[i][j]=max(fun(i+1,j,s1,s2,n,m),fun(i,j+1,s1,s2,n,m));
+    }
+}
+
 public:
-    int longestCommonSubsequence(string text1, string text2) {
-        int n=text1.size(),m=text2.size();
-        int dp[n+1][m+1];
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=m;j++){
-                dp[i][j]=0;
-            }
-        }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(text1[i-1]==text2[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
-                }
-                else dp[i][j]= max(dp[i-1][j],dp[i][j-1]);
-            }
-        }
-        return dp[n][m];
+    int longestCommonSubsequence(string s1, string s2) {
+        memset(dp,-1,sizeof(dp));
+        int n=s1.size();
+        int m=s2.size();
+        //doing recursion then memoizing
+        return fun(0,0,s1,s2,n,m);
     }
 };
