a7eb72db41174dce031c16c4e86d510237496b90
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2024-10-15 22:24:14 +0530

    [1143] [Time Beats: 95.41%] [Memory Beats: 88.56%] - LeetPush

--- 1143-Longest-Common-Subsequence.cpp
+++ 1143-Longest-Common-Subsequence.cpp
@@ -1,6 +1,6 @@
 class Solution {
 
-int dp[1001][1001];
+/*int dp[1001][1001];
 
 int fun(int i,int j,string &s1,string &s2,int n,int m){
     if(i>=n || j>=m){
@@ -14,14 +14,36 @@ int fun(int i,int j,string &s1,string &s2,int n,int m){
     }else{
         return dp[i][j]=max(fun(i+1,j,s1,s2,n,m),fun(i,j+1,s1,s2,n,m));
     }
-}
+}*/
 
 public:
     int longestCommonSubsequence(string s1, string s2) {
-        memset(dp,-1,sizeof(dp));
+        //memset(dp,-1,sizeof(dp));
         int n=s1.size();
         int m=s2.size();
-        //doing recursion then memoizing
-        return fun(0,0,s1,s2,n,m);
+        //doing recursion(2^(m+n)) then memoizing((m+1)*(n+1))
+        //now doing tabulation
+        //t[i][j]:longest common subsequence between s1 till i and s2 till j
+
+        int v[n+1][m+1];
+
+        //base cases
+        for(int i=0;i<=n;i++){
+            v[i][0]=0;
+        }
+        for(int i=0;i<=m;i++){
+            v[0][i]=0;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1]==s2[j-1]){
+                    v[i][j]=1+v[i-1][j-1];
+                }else{
+                    v[i][j]=max(v[i-1][j] , v[i][j-1]);
+                }
+            }
+        }
+        return v[n][m];
     }
 };
