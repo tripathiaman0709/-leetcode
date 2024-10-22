b8bf8a85e634e8cf4518704c67410af053cb84a1
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2024-10-22 14:09:32 +0530

    [72] [Time Beats: 5.03%] [Memory Beats: 15.71%] - LeetPush

--- 72-Edit-Distance.cpp
+++ 72-Edit-Distance.cpp
@@ -3,23 +3,23 @@ public:
     int dp[501][501];
 
     int fun(string s1,string s2,int i,int j){
-        if(i==s1.size()){
-            return s2.size()-j;
+        if(i==0){
+            return j;
         }
-        if(j==s2.size()){
-            return s1.size()-i;
+        if(j==0){
+            return i;
         }
 
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
 
-        if(s1[i]==s2[j]){
-            return dp[i][j]=fun(s1,s2,i+1,j+1);
+        if(s1[i-1]==s2[j-1]){
+            return dp[i][j]=fun(s1,s2,i-1,j-1);
         }else{
-            int ins=1+fun(s1,s2,i,j+1);
-            int del=1+fun(s1,s2,i+1,j);
-            int rep=1+fun(s1,s2,i+1,j+1);
+            int ins=1+fun(s1,s2,i,j-1);
+            int del=1+fun(s1,s2,i-1,j);
+            int rep=1+fun(s1,s2,i-1,j-1);
 
             return dp[i][j]=min({ins,del,rep});
         }
@@ -32,6 +32,6 @@ public:
 
         memset(dp,-1,sizeof(dp));
 
-        return fun(s1,s2,0,0);
+        return fun(s1,s2,n,m);
     }
 };
