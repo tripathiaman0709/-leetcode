ab88435956fa75181e1704a127602e0ce9afced8
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2024-10-23 11:30:29 +0530

    [72] [Time Beats: 66.09%] [Memory Beats: 75.45%] - LeetPush

--- 72-Edit-Distance.cpp
+++ 72-Edit-Distance.cpp
@@ -1,37 +1,21 @@
 class Solution {
 public:
-    int dp[501][501];
-
-    int fun(string s1,string s2,int i,int j){
-        if(i==0){
-            return j;
-        }
-        if(j==0){
-            return i;
-        }
-
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-
-        if(s1[i-1]==s2[j-1]){
-            return dp[i][j]=fun(s1,s2,i-1,j-1);
-        }else{
-            int ins=1+fun(s1,s2,i,j-1);
-            int del=1+fun(s1,s2,i-1,j);
-            int rep=1+fun(s1,s2,i-1,j-1);
-
-            return dp[i][j]=min({ins,del,rep});
-        }
-        return 0;
-    }
-
     int minDistance(string s1, string s2) {
         int n=s1.size();
         int m=s2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1));
 
-        memset(dp,-1,sizeof(dp));
-
-        return fun(s1,s2,n,m);
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0 || j==0){
+                    dp[i][j]=i+j;
+                }else if(s1[i-1]==s2[j-1]){
+                    dp[i][j]=dp[i-1][j-1];
+                }else{
+                    dp[i][j]=1+min({dp[i][j-1],dp[i-1][j],dp[i-1][j-1]});
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
