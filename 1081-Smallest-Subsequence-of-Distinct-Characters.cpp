ffe14d00f9b9a34d108bc37d4dff8398cb9c0ac2
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2024-10-31 11:20:12 +0530

    [1081] [Time Beats: 100.00%] [Memory Beats: 40.10%] - LeetPush

---
+++ 1081-Smallest-Subsequence-of-Distinct-Characters.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string smallestSubsequence(string s) {
+        int n=s.size();
+        vector<bool>taken(n,false);//at start didn't take any character
+        vector<int>lastind(26);
+        stack<char>ans;
+        for(int i=0;i<n;i++){
+            char ch=s[i];
+            int ind=s[i]-'a';
+            lastind[ind]=i;//stores last occuring index of each character
+        }
+
+        for(int i=0;i<n;i++){
+            char ch=s[i];
+            int ind=s[i]-'a';
+
+            if(taken[ind]){
+                continue;//if already taken then move on
+            }
+
+            while(ans.size()>0 && ans.top()>ch && lastind[ans.top()-'a']>i){
+                taken[ans.top()-'a']=false;
+                ans.pop();
+            }
+            ans.push(ch);
+            taken[ch-'a']=true;
+        }
+        string sol="";
+        while(!ans.empty()){
+            sol+=ans.top();
+            ans.pop();
+        }
+        reverse(sol.begin(),sol.end());
+        return sol;
+    }
+};
