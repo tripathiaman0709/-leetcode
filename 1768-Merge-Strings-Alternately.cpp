757fb8661516f794e602c3dfc566d6154f234d2f
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2024-11-07 16:22:08 +0530

    [1768] [Time Beats: 100.00%] [Memory Beats: 34.22%] - LeetPush

--- 1768-Merge-Strings-Alternately.cpp
+++ 1768-Merge-Strings-Alternately.cpp
@@ -1,30 +1,16 @@
 class Solution {
 public:
     string mergeAlternately(string s1, string s2) {
-        int n=s1.size();
-        int m=s2.size();
-
-        int z=min(n,m);
-
-        int l=0;
         string ans="";
-
-        while(l<z){
-            ans+=s1[l];
-            ans+=s2[l];
-            l++;
-        } 
-        if(z==m){      
-        for(int i=z;i<n;i++){
-            ans+=s1[i];
-        }
-        }else{
-        for(int i=z;i<m;i++){
-            ans+=s2[i];
-        }
+        int z=max((int)s1.size(),(int)s2.size());
+        for(int i=0;i<z;i++){
+            if(i<(int)s1.size()){
+                ans+=s1[i];
+            }
+            if(i<(int)s2.size()){
+                ans+=s2[i];
+            }
         }
         return ans;
-
-
     }
 };
