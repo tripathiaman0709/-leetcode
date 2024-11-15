a23f394aaec9b9b660cd83bba89907e61aebc59f
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2024-11-15 20:42:53 +0530

    [2064] [Time Beats: 83.82%] [Memory Beats: 72.39%] - LeetPush

--- 2064-Minimized-Maximum-of-Products-Distributed-to-Any-Store.cpp
+++ 2064-Minimized-Maximum-of-Products-Distributed-to-Any-Store.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
-    bool isPossible(int n,vector<int>&quantities,int mid){
-        int stores=0;
-        for(auto x:quantities){
-            stores+=x/mid;
-            if(x%mid) stores++;
-            if(stores>n) return 0;
-        }
-        return stores<=n;
-    }
-    int minimizedMaximum(int n, vector<int>& quantities) {
-        int s=1,e=100000,ans=-1;
-        while(s<=e){
-            int mid=s+(e-s)/2;
-            if(isPossible(n,quantities,mid)){
-                ans=mid;
-                e=mid-1;
+
+    bool fun(int x,vector<int>&v,int n){
+        int k=n;
+        for(int i=0;i<v.size();i++){
+            k-=((v[i]+x-1)/x);
+            if(k<0){
+                return false;
             }
-            else{
-                s=mid+1;
+        }
+        return true;
+    }
+
+    int minimizedMaximum(int n, vector<int>& v) {
+
+        int low=1;
+        int high=*max_element(begin(v),end(v));
+        int ans=0;
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+
+            if(fun(mid,v,n)){
+                ans=mid;
+                high=mid-1;
+            }else{
+                low=mid+1;
             }
         }
         return ans;
