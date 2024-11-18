53a9c1b4371c46860f48a7b1a9f91257e2a25137
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2024-11-18 12:48:57 +0530

    [731] [Time Beats: 5.11%] [Memory Beats: 6.10%] - LeetPush

--- 731-My-Calendar-II.cpp
+++ 731-My-Calendar-II.cpp
@@ -1,29 +1,30 @@
 class MyCalendarTwo {
 public:
 
-    vector<pair<int,int>>second;
-    vector<pair<int,int>>first;
-
+    map<int,int>mpp;
     MyCalendarTwo() {
-        
     }
     
-    bool book(int start, int end) {
-        for(auto it:second){
-            int pehla=it.first;
-            int doosra=it.second;
-            if(max(pehla,start)<min(doosra,end)){
+    bool book(int s, int e) {
+        mpp[s]++;
+        mpp[e]--;
+
+        int sum=0;
+        for(auto it:mpp){
+            sum+=it.second;
+
+            if(sum>2){
+                mpp[s]--;
+                mpp[e]++;
                 return false;
             }
         }
-        for(auto it:first){
-            int pehla=it.first;
-            int doosra=it.second;
-            
-            second.push_back({max(start,pehla),min(end,doosra)});
-        }
-
-        first.push_back({start,end});
         return true;
     }
 };
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool param_1 = obj->book(startTime,endTime);
+ */
