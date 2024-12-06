5e06cc67e52ae1accceb1e1a447ff1fdd170cf30
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2024-12-06 11:52:29 +0530

    [2554] [Time Beats: 18.70%] [Memory Beats: 24.57%] - LeetPush

---
+++ DCP-12-24/2554-Maximum-Number-of-Integers-to-Choose-From-a-Range-I.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxCount(vector<int>& v, int x, int mxsum) {
+        int n=v.size();
+        set<int>st;
+        for(int i=0;i<n;i++){
+            st.insert(v[i]);
+        }
+        int sum=0;
+        int count=0;
+        for(int i=1;i<=x;i++){
+            if(st.find(i)==st.end() && sum+i<=mxsum){
+                count++;
+                sum+=i;
+            }
+        }
+        return count;
+    }
+};
