5048a2cf619162358f426c036a7a6dbb24523e84
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2024-12-17 12:20:17 +0530

    [2182] [Time Beats: 81.01%] [Memory Beats: 39.22%] - LeetPush

--- DCP-12-24/2182-Construct-String-With-Repeat-Limit.cpp
+++ DCP-12-24/2182-Construct-String-With-Repeat-Limit.cpp
@@ -1,45 +1,43 @@
 class Solution {
 public:
-    string repeatLimitedString(string s, int repeatLimit) {
-        vector<int> count(26, 0);
-        for(char &ch : s) {
-            count[ch-'a']++;
-        }
-
-        priority_queue<char> pq;
-        for(int i = 0; i < 26; i++) {
-            if(count[i] > 0) {
-                char ch = 'a' + i;
-                pq.push(ch);
+    string repeatLimitedString(string s, int k) {
+        int n=s.size();
+        //using priority queue
+        //max heap
+        vector<int>fre(26,0);
+        for(int i=0;i<n;i++){
+            fre[s[i]-'a']++;
+        }        
+        priority_queue<char>pq;
+        for(int i=0;i<26;i++){
+            if(fre[i]>0){
+                pq.push('a'+i);
             }
         }
-
-        string result;
-        while(!pq.empty()) {
-            char ch = pq.top();
+        string ans="";
+        while(!pq.empty()){
+            char ch=pq.top();
             pq.pop();
+            int fr=min(k,fre[ch-'a']);
+            fre[ch-'a']-=fr;
+            ans.append(fr,ch);
 
-            int freq = min(count[ch-'a'], repeatLimit);
-            result.append(freq, ch);
-
-            count[ch-'a'] -= freq;
-
-            if(count[ch-'a'] > 0 && !pq.empty()) {
-                char nextChar = pq.top();
+            if(fre[ch-'a']>0 && !pq.empty()){
+                char pch=pq.top();
                 pq.pop();
 
-                result.push_back(nextChar);
-                count[nextChar-'a']--;
-
-                if(count[nextChar-'a'] > 0) {
-                    pq.push(nextChar);
+                if(fre[pch-'a']>0){
+                    fre[pch-'a']--;
+                    ans+=pch;
+                }
+                if(fre[pch-'a']>0){
+                    pq.push(pch);
                 }
 
                 pq.push(ch);
-
             }
+            
         }
-
-        return result;
+        return ans;
     }
 };
