f00397649c074b8704c1841f05333f1a1c704c1d
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2024-12-19 12:09:06 +0530

    [769] [Time Beats: 100.00%] [Memory Beats: 20.72%] - LeetPush

--- DCP-12-24/769-Max-Chunks-To-Make-Sorted.cpp
+++ DCP-12-24/769-Max-Chunks-To-Make-Sorted.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
-    int maxChunksToSorted(vector<int>& arr) {
-        int n=arr.size();
-        int cnt=0, diff=0;
-        for(int i=0; i<n; i++){
-            diff+=arr[i]-i;
-            cnt+=(diff==0);
+    int maxChunksToSorted(vector<int>& v) {
+        int n=v.size();
+        vector<int>pre(n,0);
+        for(int i=0;i<n;i++){
+            pre[i]=i;
         }
-        return cnt;
+        for(int i=1;i<n;i++){
+            pre[i]=pre[i]+pre[i-1];
+        }
+        for(int i=1;i<n;i++){
+            v[i]=v[i]+v[i-1];
+        }
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(pre[i]==v[i]){
+                count++;
+            }
+        }
+        return count;
     }
 };
