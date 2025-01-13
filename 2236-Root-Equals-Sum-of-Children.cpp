2a3e0a34ae2c40d67a52f321d03b0882d80b771e
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2025-01-13 16:46:27 +0530

    [2236] [Time Beats: 100.00%] [Memory Beats: 53.21%] - LeetPush

---
+++ 2236-Root-Equals-Sum-of-Children.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        //first easy technique
+        //making a vector and turning it into a super easy question
+
+        // vector<int>v;
+        // ListNode* temp=head;
+        // while(temp!=nullptr){
+        //     v.push_back(temp->val);
+        //     temp=temp->next;
+        // }
+        // int maxi=0;
+        // for(int i=0;i<v.size()/2;i++){
+        //     int x=v[i]+v[v.size()-1-i];
+        //     maxi=max(maxi,x);
+        // }
+        // return maxi;
+
+        //now thinking of a better approach
+        
+    }
+};
