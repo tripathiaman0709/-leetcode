4d47ade03a81f61a1ae1ba2777ce8bba8653cc4c
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2025-02-22 00:19:36 +0530

    [1261] [Time Beats: 100.00%] [Memory Beats: 80.56%] - LeetPush

--- DCP-02-25/1261-Find-Elements-in-a-Contaminated-Binary-Tree.cpp
+++ DCP-02-25/1261-Find-Elements-in-a-Contaminated-Binary-Tree.cpp
@@ -13,20 +13,40 @@ class FindElements {
 public:
     unordered_set<int>st;
 
-    void dfs(TreeNode* root,int x){
-        if(root==nullptr){
-            return;
+    // void dfs(TreeNode* root,int x){
+    //     if(root==nullptr){
+    //         return;
+    //     }
+    //     // root->val=x;
+    //     st.insert(x);
+    //     dfs(root->left,2*x+1);
+    //     dfs(root->right,2*x+2);
+    // }
+
+    void bfs(TreeNode* root,int x){
+        queue<TreeNode*>q;
+        root->val=x;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* node=q.front();
+            q.pop();
+            st.insert(node->val);
+            if(node->left){
+                node->left->val=2*node->val+1;
+                q.push(node->left);
+            }
+            if(node->right){
+                node->right->val=2*node->val+2;
+                q.push(node->right);
+            }
         }
-        // root->val=x;
-        st.insert(x);
-        dfs(root->left,2*x+1);
-        dfs(root->right,2*x+2);
     }
 
     FindElements(TreeNode* root) {
         //using simple bfs or dfs
         //as both will work
-        dfs(root,0);   
+        // dfs(root,0);   
+        bfs(root,0);
     }
     
     bool find(int target) {
