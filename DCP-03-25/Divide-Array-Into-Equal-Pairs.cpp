a40fa349a0744e08ebc825d2256860f5a8dd2c73
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2025-03-17 11:53:39 +0530

    [2206] [Time Beats: 58.51%] [Memory Beats: 84.67%] - LeetPush

--- DCP-03-25/Divide-Array-Into-Equal-Pairs.cpp
+++ DCP-03-25/Divide-Array-Into-Equal-Pairs.cpp
@@ -2,13 +2,20 @@ class Solution {
 public:
     bool divideArray(vector<int>& v) {
         int n=v.size();
-        unordered_map<int,int>mpp;
-        for(auto it:v){
-            mpp[it]++;
-        }
-        int ans=0;
-        for(auto it:mpp){
-            if(it.second%2!=0){
+        // unordered_map<int,int>mpp;
+        // for(auto it:v){
+        //     mpp[it]++;
+        // }
+        // int ans=0;
+        // for(auto it:mpp){
+        //     if(it.second%2!=0){
+        //         return false;
+        //     }
+        // }
+        // return true;
+        sort(v.begin(),v.end());
+        for(int i=0;i<n;i+=2){
+            if(v[i]!=v[i+1]){
                 return false;
             }
         }
