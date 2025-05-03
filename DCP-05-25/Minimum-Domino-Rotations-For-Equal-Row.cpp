11540d87fe23d4613dba8bae58225fb62703ee36
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2025-05-03 12:25:49 +0530

    [1007] [Time Beats: 21.55%] [Memory Beats: 48.71%] - LeetPush

---
+++ DCP-05-25/Minimum-Domino-Rotations-For-Equal-Row.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+
+    int fun(vector<int>&t,vector<int>&b,int v){
+        int tmin=0;
+        int bmin=0;
+        for(int i=0;i<t.size();i++){    
+            if(t[i]!=v && b[i]!=v){
+                return -1;
+            }else if(t[i]==v){
+                continue;
+            }else{
+                tmin++;
+            }
+        }
+        for(int i=0;i<t.size();i++){    
+            if(t[i]!=v && b[i]!=v){
+                return -1;
+            }else if(b[i]==v){
+                continue;
+            }else{
+                bmin++;
+            }
+        }
+        return min(tmin,bmin);
+    }
+
+    int minDominoRotations(vector<int>& t, vector<int>& b) {
+        int n=t.size();
+        //saari values ke liye brute force way me check krlo 
+        //as only six possible values hi toh hai
+        int ans=INT_MAX;
+        for(int i=1;i<=6;i++){
+            int val=fun(t,b,i);
+            if(val!=-1){
+                ans=min(ans,val);
+            }
+        }
+        if(ans==INT_MAX){
+            return -1;
+        }
+        return ans;
+    }
+};
