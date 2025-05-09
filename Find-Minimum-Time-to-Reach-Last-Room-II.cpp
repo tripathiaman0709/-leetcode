7885676be534b287502b2e7600cecede848b9ab3
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2025-05-09 22:10:18 +0530

    [3342] [Time Beats: 92.28%] [Memory Beats: 94.39%] - LeetPush

---
+++ Find-Minimum-Time-to-Reach-Last-Room-II.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    int minTimeToReach(vector<vector<int>>& grid) {
+        //hmm priority queue lagana hoga
+        //as greedily toh solve nhi ho paayega
+
+        int n=grid.size();
+        int m=grid[0].size();
+
+        //chlo dijkstra's algorithm lagaya jaaye
+        //initialising a minimum priority queue
+
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> pq;
+
+        pq.push({0,{0,0}});
+        //distance , coordinates -> i,j
+
+        int dx[] = {0, 0, 1, -1};
+        int dy[] = {1, -1, 0, 0};
+
+        vector<vector<int>>finalgrid(n,vector<int>(m,INT_MAX));
+        finalgrid[0][0]=0;
+        //source
+
+        while(!pq.empty()){
+            int cost=pq.top().first;
+            int x=pq.top().second.first;
+            int y=pq.top().second.second;
+            
+            pq.pop();
+
+            if(x==n-1 && y==m-1){
+                return cost;
+            }
+
+            for (int i = 0; i < 4; ++i) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+                    int steps=nx+ny;
+                    int add=0;
+                    if(steps%2==0){
+                        add=2;
+                    }else{
+                        add=1;
+                    }
+                    int wait=grid[nx][ny]-cost;
+                    int newcost=0;
+                    if(wait>0){
+                        newcost=wait+cost+add;
+                    }                    
+                    else{
+                       newcost=cost+add; 
+                    }
+                    if (newcost < finalgrid[nx][ny]) {
+                        finalgrid[nx][ny] = newcost;
+                        pq.push({newcost, {nx, ny}});
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+};
