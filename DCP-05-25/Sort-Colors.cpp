ac20349d4974eba3272f863043427bf63f2272f6
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2025-05-17 11:53:26 +0530

    [75] [Time Beats: 100.00%] [Memory Beats: 97.40%] - LeetPush

--- DCP-05-25/Sort-Colors.cpp
+++ DCP-05-25/Sort-Colors.cpp
@@ -1,23 +1,44 @@
 class Solution {
 public:
     void sortColors(vector<int>& v) {
+        // int n=v.size();
+        // //soving without sorting
+        // vector<int>hash(3,0);
+        // for(int i=0;i<n;i++){
+        //     hash[v[i]]++;
+        // }
+        // vector<int>ans;
+        // for(int i=0;i<hash[0];i++){
+        //     ans.push_back(0);
+        // }
+        // for(int i=0;i<hash[1];i++){
+        //     ans.push_back(1);
+        // }
+        // for(int i=0;i<hash[2];i++){
+        //     ans.push_back(2);
+        // }
+
+        // v=ans;
+
+        // sort(v.begin(),v.end());
+
+        //doing the code in constant space complexity
+
         int n=v.size();
-        //soving without sorting
-        vector<int>hash(3,0);
-        for(int i=0;i<n;i++){
-            hash[v[i]]++;
-        }
-        vector<int>ans;
-        for(int i=0;i<hash[0];i++){
-            ans.push_back(0);
-        }
-        for(int i=0;i<hash[1];i++){
-            ans.push_back(1);
-        }
-        for(int i=0;i<hash[2];i++){
-            ans.push_back(2);
+        int l=0;
+        int r=n-1;
+        int i=0;
+        while(i<=r){
+            if(v[i]==0){
+                swap(v[l],v[i]);
+                l++;
+            }else if(v[i]==2){
+                swap(v[r],v[i]);
+                r--;
+                i--;
+            }
+            i++;
         }
 
-        v=ans;
     }
 };
