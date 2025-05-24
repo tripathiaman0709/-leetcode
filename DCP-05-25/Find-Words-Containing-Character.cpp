c62ae6feb7f58cdcaabe8f357495b6e024d8179c
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2025-05-24 13:42:55 +0530

    [2942] [Time Beats: 100.00%] [Memory Beats: 28.80%] - LeetPush

--- DCP-05-25/Find-Words-Containing-Character.cpp
+++ DCP-05-25/Find-Words-Containing-Character.cpp
@@ -3,13 +3,11 @@ public:
     vector<int> findWordsContaining(vector<string>& v, char x) {
         int n=v.size();
         vector<int>ans;
-        for(int i=0;i<n;i++){
-            string s=v[i];
-            sort(s.begin(),s.end());
-            if(binary_search(s.begin(),s.end(),x)){
-                ans.push_back(i);
-            }
+        for (int i = 0; i < v.size(); ++i) {
+        if (find(v[i].begin(), v[i].end(), x) != v[i].end()) {
+            ans.push_back(i);
         }
+    }
         return ans;
     }
 };
