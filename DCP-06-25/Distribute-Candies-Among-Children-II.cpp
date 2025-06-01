9020951cd9cc3c0977d4e921c092646b6ead3075
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2025-06-01 16:45:26 +0530

    [2929] [Time Beats: 33.53%] [Memory Beats: 84.71%] - LeetPush

--- DCP-06-25/Distribute-Candies-Among-Children-II.cpp
+++ DCP-06-25/Distribute-Candies-Among-Children-II.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     long long distributeCandies(int n, int l) {
         long long ans=0;
-        int min1=max(0,n-2*l);
-        int max1=min(n,l);
+        // int min1=max(0,n-2*l);
+        // int max1=min(n,l);
 
-        for(int i=min1;i<=max1;i++){
-            int N=max(0,n-i);
-            int min2=max(0,N-l);
-            int max2=min(N,l);
+        for(int i=max(0,n-2*l);i<=min(n,l);i++){
+            // int N=max(0,n-i);
+            // int min2=max(0,max(0,n-i)-l);
+            // int max2=min(max(0,n-i),l);
 
-            ans+=max2-min2+1;
+            ans+=min(max(0,n-i),l)-max(0,max(0,n-i)-l)+1;
         }
 
         return ans;
