e254dff01dad25f9316995c40423f372daa4a48a
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2025-06-03 12:36:14 +0530

    [1402] [Time Beats: 48.58%] [Memory Beats: 39.56%] - LeetPush

--- Reducing-Dishes.cpp
+++ Reducing-Dishes.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
+    int maxSatisfaction(vector<int>& v) {
+        int n = v.size();
+        sort(v.begin(), v.end());
 
-    int fun(vector<int>& v, int ind, int time,vector<vector<int>>&dp) {
-        if (ind == v.size()) return 0;
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
 
-        if(dp[ind][time]!=-1){
-            return dp[ind][time];
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int time = ind; time >= 0; time--) {
+                int pick = v[ind] * (time + 1) + dp[ind + 1][time + 1];
+                int notPick = dp[ind + 1][time];
+                dp[ind][time] = max(pick, notPick);
+            }
         }
 
-        int pick = v[ind] * (time + 1) + fun(v, ind + 1, time + 1,dp);
-
-        int notPick = fun(v, ind + 1, time,dp);
-
-        return dp[ind][time]=max(pick, notPick);
-    }
-
-    int maxSatisfaction(vector<int>& v) {
-        int n=v.size();
-        sort(v.begin(), v.end()); 
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
-        return fun(v, 0, 0,dp); 
+        return dp[0][0];
     }
 };
