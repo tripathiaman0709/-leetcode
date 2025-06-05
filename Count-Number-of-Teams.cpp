cccb1e722fe47793635d93563d9a147ac61d481d
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2025-06-06 03:04:42 +0530

    [1395] [Time Beats: 85.60%] [Memory Beats: 95.69%] - LeetPush

--- Count-Number-of-Teams.cpp
+++ Count-Number-of-Teams.cpp
@@ -1,40 +1,32 @@
 class Solution {
 public:
-    int n;
-    int t1[100001][3];
-    int t2[100001][3];
-    int solve_inc(vector<int>&rating,int i,int count){
-        if(count==2) return 1;
-        if(i>=n) return 0;
-        if(t1[i][count]!=-1) return t1[i][count];
-        int ans=0;
-        for(int j=i+1;j<n;j++){
-            if(rating[j]>rating[i]){
-                ans+=solve_inc(rating,j,count+1);
+    int numTeams(vector<int>& a) {
+        int n=a.size();
+        int sum=0;
+
+        for(int i=1;i<n-1;i++){
+            int leftsmaller=0;
+            int rightlarger=0;
+
+            for(int j=0;j<i;j++){
+                if(a[j]<a[i]){
+                    leftsmaller++;
+                }
             }
-        }
-        return t1[i][count]=ans;
-    }
-    int solve_dec(vector<int>&rating,int i,int count){
-        if(count==2) return 1;
-        if(i>=n) return 0;
-        if(t2[i][count]!=-1) return t2[i][count];
-        int ans=0;
-        for(int j=i+1;j<n;j++){
-            if(rating[j]<rating[i]){
-                ans+=solve_dec(rating,j,count+1);
+            for(int j=i+1;j<n;j++){
+                if(a[j]>a[i]){
+                    rightlarger++;
+                }
             }
+
+            sum+=leftsmaller*rightlarger;
+
+
+            int leftlarger=i-leftsmaller;
+            int rightsmaller=n-i-rightlarger-1;
+            sum+=leftlarger*rightsmaller;
         }
-        return t2[i][count]=ans;
-    }
-    int numTeams(vector<int>& rating) {
-        memset(t1,-1,sizeof(t1));
-        memset(t2,-1,sizeof(t2));
-        n=rating.size();
-        int ans=0;
-        for(int i=0;i<n;i++){
-            ans+=solve_inc(rating,i,0)+solve_dec(rating,i,0);
-        }
-        return ans;
+        return sum;
+        
     }
 };
