f1f3e0ad7c34297166a5be1558635e7c142b70c1
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2025-06-08 15:43:10 +0530

    [386] [Time Beats: 27.30%] [Memory Beats: 44.11%] - LeetPush

--- DCP-06-25/Lexicographical-Numbers.cpp
+++ DCP-06-25/Lexicographical-Numbers.cpp
@@ -1,26 +1,51 @@
 class Solution {
 public:
-    vector<int> lexicalOrder(int n) {
-        //simplest approach
 
-        vector<string>v;
-
-        for(int i=1;i<=n;i++){
-            string s=to_string(i);
-
-            v.push_back(s);
+    void dfs(int x,int n,vector<int>&v){
+        if(x>n){
+            return;
         }
+        v.push_back(x);
 
-        sort(v.begin(),v.end());
+        for(int i=0;i<=9;i++){
+            int newx=10*x+i;
+            dfs(newx,n,v);
+        }
+        
+    }
 
+    vector<int> lexicalOrder(int n) {
+        // //simplest approach
+
+        // vector<string>v;
+
+        // for(int i=1;i<=n;i++){
+        //     string s=to_string(i);
+
+        //     v.push_back(s);
+        // }
+
+        // sort(v.begin(),v.end());
+
+        // vector<int>ans;
+
+        // for(int i=0;i<v.size();i++){
+        //     int x=stoi(v[i]);
+
+        //     ans.push_back(x);
+        // }
+
+        // return ans;
+
+
+        //better approach
+        //using dfs
         vector<int>ans;
-
-        for(int i=0;i<v.size();i++){
-            int x=stoi(v[i]);
-
-            ans.push_back(x);
+        for(int i=1;i<=9;i++){
+            dfs(i,n,ans);
         }
 
         return ans;
+
     }
 };
