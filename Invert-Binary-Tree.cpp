db8ae92c48d5c6dd7749fa7199acdf8dc588ece6
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2025-06-14 20:32:59 +0530

    [226] [Time Beats: 100.00%] [Memory Beats: 59.53%] - LeetPush

---
+++ Invert-Binary-Tree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        //matlab mirror krna hai
+        //swapping hogi
+        //left node right ban jaayegi and vice versa
+
+        //base case
+        if(root==nullptr){
+            return root;
+        }
+        TreeNode* temp=root->left;
+        root->left=root->right;
+        root->right=temp;
+
+        invertTree(root->left);
+        invertTree(root->right);
+
+        return root;
+
+    }
+};
