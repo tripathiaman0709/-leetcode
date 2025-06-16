a9f29745a5bf2a1c4f3904a93d51389b145ffcd2
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2025-06-16 11:17:00 +0530

    [2016] [Time Beats: 100.00%] [Memory Beats: 5.67%] - LeetPush

--- DCP-06-25/Maximum-Difference-Between-Increasing-Elements.cpp
+++ DCP-06-25/Maximum-Difference-Between-Increasing-Elements.cpp
@@ -5,13 +5,29 @@ public:
 
         int ans=-1;
 
+        // for(int i=0;i<n-1;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(v[j]>v[i]){
+        //             ans=max(ans,v[j]-v[i]);
+        //         }
+        //     }
+        // }
+
+        //trying to minimize the time complexity
+        //thinking of the greatest/largest element to the right of each element
+
+        vector<int>maxright(n,v[n-1]);
+
+        for(int i=n-2;i>=0;i--){
+            maxright[i]=max(maxright[i+1],v[i]);
+        }
+
         for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(v[j]>v[i]){
-                    ans=max(ans,v[j]-v[i]);
-                }
+            if(maxright[i]>v[i]){
+                ans=max(maxright[i]-v[i],ans);
             }
         }
+        
         return ans;
     }
 };
