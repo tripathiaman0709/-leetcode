5b596ba76e57e61efc4da291d08ef14881ea6302
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2025-06-16 11:41:24 +0530

    [2016] [Time Beats: 100.00%] [Memory Beats: 71.76%] - LeetPush

--- DCP-06-25/Maximum-Difference-Between-Increasing-Elements.cpp
+++ DCP-06-25/Maximum-Difference-Between-Increasing-Elements.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
-    int maximumDifference(vector<int>& v) {
-        int n=v.size();
-
-        int ans=-1;
-
+    int maximumDifference(vector<int>& v) {        
         // for(int i=0;i<n-1;i++){
         //     for(int j=i+1;j<n;j++){
         //         if(v[j]>v[i]){
@@ -16,18 +12,33 @@ public:
         //trying to minimize the time complexity
         //thinking of the greatest/largest element to the right of each element
 
-        vector<int>maxright(n,v[n-1]);
+        // vector<int>maxright(n,v[n-1]);
 
-        for(int i=n-2;i>=0;i--){
-            maxright[i]=max(maxright[i+1],v[i]);
-        }
+        // for(int i=n-2;i>=0;i--){
+        //     maxright[i]=max(maxright[i+1],v[i]);
+        // }
 
-        for(int i=0;i<n-1;i++){
-            if(maxright[i]>v[i]){
-                ans=max(maxright[i]-v[i],ans);
-            }
+        // for(int i=0;i<n-1;i++){
+        //     if(maxright[i]>v[i]){
+        //         ans=max(maxright[i]-v[i],ans);
+        //     }
+        // }
+
+        //now thinking of reducing the space complexity
+        //as taking O(n) space
+
+        //could we think of two pointer approach or something like that
+
+        int xMin=v[0], n=v.size();
+        int maxD=-1;
+        for(int i=1; i<n; i++){
+            int x=v[i];
+            if (x<=xMin)
+                xMin=x;
+            else maxD=max(maxD, x-xMin); 
         }
+        return maxD;
         
-        return ans;
+        // return ans;
     }
 };
