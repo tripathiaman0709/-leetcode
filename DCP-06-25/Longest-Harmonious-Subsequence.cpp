5480c550742b524cd87e1ee35999cf7116ac2bfe
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2025-06-30 11:43:48 +0530

    [594] [Time Beats: 78.03%] [Memory Beats: 72.12%] - LeetPush

--- DCP-06-25/Longest-Harmonious-Subsequence.cpp
+++ DCP-06-25/Longest-Harmonious-Subsequence.cpp
@@ -4,14 +4,33 @@ public:
         int n=v.size();
 
         sort(v.begin(),v.end());
+        // int maxi=0;
+        // for(int i=0;i<n-1;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(v[j]-v[i]==1){
+        //             maxi=max(maxi,j-i+1);
+        //         }
+        //     }
+        // }
+        // return maxi;
+        if(n==1){
+            return 0;
+        }
+        int l=0;
+        int r=0;
         int maxi=0;
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(v[j]-v[i]==1){
-                    maxi=max(maxi,j-i+1);
-                }
+        while(r<n){
+            if(v[r]-v[l]==1){
+                maxi=max(maxi,r-l+1);
+                r++;
+            }else if(v[r]-v[l]<1){
+                r++;
+            }else if(v[r]-v[l]>1){
+                l++;
             }
         }
+
         return maxi;
+
     }
 };
