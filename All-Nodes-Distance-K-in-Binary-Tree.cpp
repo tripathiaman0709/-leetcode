1a376d0e8c4e9a896ba9fb1c7a6334aad111c23e
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2025-07-02 13:49:57 +0530

    [863] [Time Beats: 21.44%] [Memory Beats: 34.90%] - LeetPush

---
+++ All-Nodes-Distance-K-in-Binary-Tree.cpp
@@ -0,0 +1,85 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+
+    void fun(TreeNode* node,unordered_map<TreeNode*,TreeNode*>&mpp){
+        if(node==nullptr){
+            return;
+        }
+        //doing level order traversal bfs style
+
+        queue<TreeNode*>q;
+        q.push(node);
+
+        while(!q.empty()){
+
+                TreeNode* it=q.front();
+                q.pop();
+
+                if(it->left!=nullptr){
+                    mpp[it->left]=it;
+                    q.push(it->left);
+                }
+                if(it->right!=nullptr){
+                    mpp[it->right]=it;
+                    q.push(it->right);
+                }
+
+        }
+    }
+
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        //finding all the nodes at distance k from the target node
+
+        unordered_map<TreeNode*,TreeNode*>mpp;
+        fun(root,mpp);
+        //made the map to connect all nodes to their parents
+        unordered_map<TreeNode*,bool>vis;
+        
+        queue<TreeNode*>q;
+        q.push(target);
+        vis[target]=true;
+        int count=0;
+
+        while(!q.empty()){
+            int sz=q.size();
+            if(count==k){
+                break;
+            }else{
+                count++;
+            }
+
+            for(int i=0;i<sz;i++){
+                TreeNode* it=q.front();
+                q.pop();
+
+                if(it->left!=nullptr && vis[it->left]==false){
+                    vis[it->left]=true;
+                    q.push(it->left);
+                }
+                if(it->right!=nullptr && vis[it->right]==false){
+                    vis[it->right]=true;
+                    q.push(it->right);
+                }
+                if(mpp[it] && vis[mpp[it]]==false){
+                    vis[mpp[it]]=true;
+                    q.push(mpp[it]);
+                }
+            }
+        }
+        vector<int>ans;
+            while(!q.empty()){
+                ans.push_back(q.front()->val);
+                q.pop();
+            }
+            return ans;
+    }
+};
