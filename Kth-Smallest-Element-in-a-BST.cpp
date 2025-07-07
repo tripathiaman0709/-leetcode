b9529eb0eebf5f7a7ca845c2ce9259ed55298afe
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2025-07-07 23:07:34 +0530

    [230] [Time Beats: 100.00%] [Memory Beats: 7.26%] - LeetPush

---
+++ Kth-Smallest-Element-in-a-BST.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    void inorder(TreeNode* node,vector<int>&v){
+        //left root right
+
+        if(node==nullptr){
+            return;
+        }
+        inorder(node->left,v);
+        v.push_back(node->val);
+        inorder(node->right,v);
+
+    }
+
+    int kthSmallest(TreeNode* root, int k) {
+        // queue<TreeNode*>q;
+        // vector<int>v;
+
+        // q.push(root);
+        // // v.push_back(root->val);
+        // while(!q.empty()){
+        //     int sz=q.size();
+        //     for(int i=0;i<sz;i++){
+        //         TreeNode* it=q.front();
+        //         q.pop();
+        //         v.push_back(it->val);
+
+        //         if(it->left!=nullptr){
+        //             q.push(it->left);
+        //         }
+        //         if(it->right!=nullptr){
+        //             q.push(it->right);
+        //         }
+        //     }
+        // }
+        // sort(v.begin(),v.end());
+
+        // return v[k-1];
+        //we know that inorder traversal of a bst gives a sorted array
+
+        vector<int>v;
+        inorder(root,v);
+
+        return v[k-1];
+        
+    }
+};
