55552f3195dded1e05d69780d8a794a43dd27be8
tripathiaman0709 <144323288+tripathiaman0709@users.noreply.github.com>
2025-09-07 11:03:53 +0530

    [1304] [Time Beats: 100.00%] [Memory Beats: 0.88%] - LeetPush

---
+++ Find-N-Unique-Integers-Sum-up-to-Zero.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        vector<int>v;
+        if(n%2==0){
+            for(int i=1;i<=n/2;i++){
+                v.push_back(i);
+            }
+            for(int i=1;i<=n/2;i++){
+                v.push_back(-1*i);
+            }
+        }else{
+            for(int i=1;i<=n/2;i++){
+                v.push_back(i);
+            }
+            v.push_back(0);
+            for(int i=1;i<=n/2;i++){
+                v.push_back(-1*i);
+            }
+        }
+        return v;
+    }
+};
