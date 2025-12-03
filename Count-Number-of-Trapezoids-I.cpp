de84da4773c51429c4a6fb7d654a7a2a6d72fa21
Aman Tripathi <144323288+tripathiaman0709@users.noreply.github.com>
2025-12-03 12:46:15 +0530

    [3623] [Time Beats: 76.27%] [Memory Beats: 81.87%] - LeetPush

--- Count-Number-of-Trapezoids-I.cpp
+++ Count-Number-of-Trapezoids-I.cpp
@@ -1,41 +1,37 @@
-class Solution {
-public:
-
-    long long M=1e9+7;
-
-    int countTrapezoids(vector<vector<int>>& v) {
-        //we know that slope is y2-y1 / x2-x1
-        //and we need equal slopes 
-        //and need to be horizontal as well
-        //ie slope is 0
-        //y2-y1 = 0
-
-        int n=v.size();
-        unordered_map<int,int>mppy;
-
-        for(int i=0;i<n;i++){
-            int y=v[i][1];
-            mppy[y]++;
-        }
-
-        for(auto &it:mppy){
-            int x=it.second;
-            int val = (1LL * x * (x - 1) / 2) % M;
-
-            it.second=val;
-        }
-
-        long long total=0;
-        long long ans=0;
-
-        for(auto it:mppy){
-            int y=it.second;
-            ans+=y*total;
-            ans%=M;
-            total+=y;
-            total%=M;
-        }
-
-        return (int)ans;
-    }
-};
+1class Solution {
+2public:
+3
+4    long long M=1e9+7;
+5
+6    int countTrapezoids(vector<vector<int>>& v) {
+7        
+8
+9        int n=v.size();
+10        unordered_map<int,int>mppy;
+11
+12        for(int i=0;i<n;i++){
+13            int y=v[i][1];
+14            mppy[y]++;
+15        }
+16
+17        for(auto &it:mppy){
+18            int x=it.second;
+19            int val = (1LL * x * (x - 1) / 2) % M;
+20
+21            it.second=val;
+22        }
+23
+24        long long total=0;
+25        long long ans=0;
+26
+27        for(auto it:mppy){
+28            int y=it.second;
+29            ans+=y*total;
+30            ans%=M;
+31            total+=y;
+32            total%=M;
+33        }
+34
+35        return (int)ans;
+36    }
+37};
