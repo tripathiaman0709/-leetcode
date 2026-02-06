ce405cf60bcbde2f67abc55ef2f5bf02401dfd14
Aman Tripathi <144323288+tripathiaman0709@users.noreply.github.com>
2026-02-06 13:49:05 +0530

    [3634] [Time Beats: 67.44%] [Memory Beats: 52.91%] - LeetPush

--- Minimum-Removals-to-Balance-Array.cpp
+++ Minimum-Removals-to-Balance-Array.cpp
@@ -1,35 +1,18 @@
-class Solution {
-public:
-    int minRemoval(vector<int>& v, int k) {
-        int n=v.size();
-        sort(v.begin(),v.end());
-
-
-        int i=0;
-        int j=1;
-        if(n==1){
-            j=i;
-        }
-        int mini=min(v[i],v[j]);
-        int maxi=max(v[i],v[j]);
-
-        int ans=INT_MAX;
-
-
-        while(j<n){
-            while((long long)k*mini<(long long)maxi){
-                i++;
-                mini=v[i];
-            }
-            ans=min(ans,n-(j-i+1));
-            j++;
-            if(i<n){
-                mini=v[i];
-            }
-            if(j<n){
-                maxi=v[j];
-            }
-        }
-        return ans;
-    }
-};
+1class Solution {
+2public:
+3    int minRemoval(vector<int>& nums, int k) {
+4        sort(nums.begin(), nums.end());
+5        int n = nums.size();
+6        int i = 0;
+7        int maxLen = 0;
+8        
+9        for (int j = 0; j < n; j++) {
+10            while ((long long)nums[j] > (long long)nums[i] * k) {
+11                i++;
+12            }
+13            maxLen = max(maxLen, j - i + 1);
+14        }
+15        
+16        return n - maxLen;
+17    }
+18};
