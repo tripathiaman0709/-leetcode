3af13276abeed04a4020b2112614e55b7ff22c95
Aman Tripathi <144323288+tripathiaman0709@users.noreply.github.com>
2026-02-15 15:48:57 +0530

    [3843] [Time Beats: 67.49%] [Memory Beats: 67.37%] - LeetPush

--- First-Element-with-Unique-Frequency.cpp
+++ First-Element-with-Unique-Frequency.cpp
@@ -1,27 +1,20 @@
 1class Solution {
 2public:
-3    int firstUniqueFreq(vector<int>& v) {
-4        int n=v.size();
-5        unordered_map<int,int>mpp;
-6        for(int i=0;i<n;i++){
-7            mpp[v[i]]++;
-8        }
-9        //now finding unique frequency
-10        vector<int>freq;
-11        for(auto it:mpp){
-12            freq.push_back(it.second);
+3    int firstUniqueFreq(vector<int>& nums) {
+4        unordered_map<int, int> mpp;
+5        unordered_map<int, int> freq;
+6
+7        for(int i=0; i<nums.size(); i++){
+8            mpp[nums[i]]++;
+9        }
+10
+11        for(auto &it: mpp){
+12            freq[it.second]++;
 13        }
-14        sort(freq.begin(),freq.end());
-15        for(int i=0;i<n;i++){
-16            int f=mpp[v[i]];
-17
-18            auto lb=lower_bound(freq.begin(),freq.end(),f)-freq.begin();
-19            auto ub=upper_bound(freq.begin(),freq.end(),f)-freq.begin()-1;
-20
-21            if(lb==ub){
-22                return v[i];
-23            }
-24        }
-25        return -1;
-26    }
-27};
+14
+15        for(auto it: nums){
+16            if(freq[mpp[it]] == 1) return it;
+17        }
+18        return -1;
+19    }
+20};
