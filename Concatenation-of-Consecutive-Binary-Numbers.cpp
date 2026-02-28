ee0505e31e2a93eaadc6a2649d0a1c6cbf276743
Aman Tripathi <144323288+tripathiaman0709@users.noreply.github.com>
2026-03-01 00:40:15 +0530

    [1680] [Time Beats: 56.38%] [Memory Beats: 17.45%] - LeetPush

--- Concatenation-of-Consecutive-Binary-Numbers.cpp
+++ Concatenation-of-Consecutive-Binary-Numbers.cpp
@@ -1,38 +1,17 @@
 1class Solution {
-2public:
-3
-4    string solve(int n){
-5        string ans="";
-6        for(int i=1;i<=n;i++){
-7            string s="";
-8            int num=i;
-9            while(num){
-10                if(num%2)s+='1';
-11                else s+='0';
-12                num/=2;
-13            }
-14            reverse(s.begin(),s.end());
-15            ans+=s;
-16        }
-17        return ans;
-18    }
-19
-20    int fun(string s){
-21        long long mod=1e9+7;
-22        long long sum=0;
-23        long long power=1;
-24
-25        for(int i=0;i<s.size();i++){
-26            if(s[s.size()-1-i]=='1'){
-27                sum=(sum+power)%mod;
-28            }
-29            power=(power*2)%mod;
-30        }
-31        return sum;
-32    }
-33
-34    int concatenatedBinary(int n) {
-35        string s=solve(n);
-36        return fun(s);
-37    }
-38};
+2 public:
+3  int concatenatedBinary(int n) {
+4    constexpr int kMod = 1'000'000'007;
+5    long ans = 0;
+6
+7    for (int i = 1; i <= n; ++i)
+8      ans = ((ans << numberOfBits(i)) % kMod + i) % kMod;
+9
+10    return ans;
+11  }
+12
+13 private:
+14  int numberOfBits(int n) {
+15    return log2(n) + 1;
+16  }
+17};
