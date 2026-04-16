775ecb35e8937b6f52f5a7a195b8427bea9eb845
Aman Tripathi <144323288+tripathiaman0709@users.noreply.github.com>
2026-04-16 13:03:34 +0530

    [3488] [Time Beats: 64.68%] [Memory Beats: 49.25%] - LeetPush

--- Closest-Equal-Element-Queries.cpp
+++ Closest-Equal-Element-Queries.cpp
@@ -1,37 +1,41 @@
 1class Solution {
 2public:
-3    vector<int> solveQueries(vector<int>& vec, vector<int>& q) {
-4        int n=vec.size();
-5        vector<int>v;
-6        for(auto it:vec){
-7            v.push_back(it);
-8        }
-9        for(auto it:vec){
-10            v.push_back(it);
-11        }
-12        for(auto it:vec){
-13            v.push_back(it);
-14        }
-15
-16        //using set
-17        //but normal set and not unordered as we require the positions as well
-18        set<pair<int,int>>st;
-19        vector<int>ans;
-20        for(int i=0;i<v.size();i++){
-21            st.insert({v[i],i});
-22        }
-23        for(auto it:q){
-24            int x=n+it;
+3    vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
+4        int n = nums.size();
+5        unordered_map<int, vector<int>> mp;
+6
+7        // store indices
+8        for (int i = 0; i < n; i++) {
+9            mp[nums[i]].push_back(i);
+10        }
+11
+12        vector<int> ans;
+13
+14        for (int q : queries) {
+15            vector<int>& v = mp[nums[q]];
+16
+17            // only one occurrence
+18            if (v.size() == 1) {
+19                ans.push_back(-1);
+20                continue;
+21            }
+22
+23            int pos = lower_bound(v.begin(), v.end(), q) - v.begin();
+24            int res = INT_MAX;
 25
-26            auto pos=st.lower_bound({v[x],x});
-27            int z=min({n,x-prev(pos)->second,next(pos)->second-x});
-28            if(z<n){
-29                ans.push_back(z);
-30            }else{
-31                ans.push_back(-1);
-32            }
-33        }
-34        return ans;
+26            // left neighbor
+27            int left = v[(pos - 1 + v.size()) % v.size()];
+28            int d1 = abs(q - left);
+29            res = min(res, min(d1, n - d1));
+30
+31            // right neighbor
+32            int right = v[(pos + 1) % v.size()];
+33            int d2 = abs(q - right);
+34            res = min(res, min(d2, n - d2));
 35
-36    }
-37};
+36            ans.push_back(res);
+37        }
+38
+39        return ans;
+40    }
+41};
