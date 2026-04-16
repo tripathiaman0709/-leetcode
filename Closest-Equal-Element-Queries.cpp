60f711bb06854722ae6a22c00fe0e9bd1ebde6b2
Aman Tripathi <144323288+tripathiaman0709@users.noreply.github.com>
2026-04-16 12:58:06 +0530

    [3488] [Time Beats: 5.47%] [Memory Beats: 5.47%] - LeetPush

--- Closest-Equal-Element-Queries.cpp
+++ Closest-Equal-Element-Queries.cpp
@@ -1,24 +1,37 @@
-class Solution {
-public: 
-    vector<int> solveQueries(vector<int>& nums, vector<int>& queries) {
-        vector<int> ans(queries.size(),-1);
-        unordered_map<int,vector<int>> mp;
-        for(int i=0;i<nums.size();i++){
-            mp[nums[i]].push_back(i);
-        }
-        int sz=nums.size();
-        for(int i=0;i<queries.size();i++){
-            int n=nums[queries[i]];
-            if(mp[n].size()<2)continue;
-            auto it = lower_bound(mp[n].begin(), mp[n].end(), queries[i]);
-            int t=mp[n].size();
-            int cur=it-mp[n].begin();
-            int x=mp[n][(cur-1+t)%t];
-            int y=mp[n][(cur+1)%t];
-            int z=mp[n][cur];
-            int dist1=min((z-x+sz)%sz,(x-z+sz)%sz), dist2=min((z-y+sz)%sz,(y-z+sz)%sz);
-            ans[i]=min(dist1,dist2);
-        }
-        return ans;
-    }
-};
+1class Solution {
+2public:
+3    vector<int> solveQueries(vector<int>& vec, vector<int>& q) {
+4        int n=vec.size();
+5        vector<int>v;
+6        for(auto it:vec){
+7            v.push_back(it);
+8        }
+9        for(auto it:vec){
+10            v.push_back(it);
+11        }
+12        for(auto it:vec){
+13            v.push_back(it);
+14        }
+15
+16        //using set
+17        //but normal set and not unordered as we require the positions as well
+18        set<pair<int,int>>st;
+19        vector<int>ans;
+20        for(int i=0;i<v.size();i++){
+21            st.insert({v[i],i});
+22        }
+23        for(auto it:q){
+24            int x=n+it;
+25
+26            auto pos=st.lower_bound({v[x],x});
+27            int z=min({n,x-prev(pos)->second,next(pos)->second-x});
+28            if(z<n){
+29                ans.push_back(z);
+30            }else{
+31                ans.push_back(-1);
+32            }
+33        }
+34        return ans;
+35
+36    }
+37};
