68bdedc4d07ee819151ed8951334826ae322e8c4
Aman Tripathi <144323288+tripathiaman0709@users.noreply.github.com>
2026-05-07 03:14:34 +0530

    [1861] [Time Beats: 42.27%] [Memory Beats: 69.08%] - LeetPush

--- Rotating-the-Box.cpp
+++ Rotating-the-Box.cpp
@@ -1,34 +1,43 @@
 1class Solution {
 2public:
-3    vector<vector<char>> rotateTheBox(vector<vector<char>>& grid) {
-4        int n=grid.size();
-5        int m=grid[0].size();
+3    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
+4        int m = box.size();
+5        int n = box[0].size();
 6
-7        for(int i=0;i<n;i++){
-8            for(int j=m-2;j>=0;j--){
-9                if(grid[i][j]=='#'){
-10                    int right=j+1;
-11                    bool billu=false;
-12                    while(right <m && grid[i][right]=='.'){
-13                        right++;
-14                        billu=true;
-15                    }
-16                    if(billu){
-17                        grid[i][right-1]='#';
-18                        grid[i][j]='.';
-19                    }
-20                }
-21            }
-22        }
-23
-24        vector<vector<char>>ans(m,vector<char>(n,'.'));
-25
-26        for(int i=0;i<n;i++){
-27            for(int j=0;j<m;j++){
-28                ans[j][n-i-1]=grid[i][j];
-29            }
-30        }
+7        vector<vector<char>> result(n, vector<char>(m));
+8
+9        //Transpose
+10        for(int i = 0; i < n; i++) {
+11            for(int j = 0; j < m; j++) {
+12                result[i][j] = box[j][i];
+13            }
+14        }
+15
+16        //90 degree k lie - Reverse kardo ahr row ko
+17        for(vector<char>& row : result) {
+18            reverse(begin(row), end(row));
+19        }
+20
+21
+22        //Apply the gravity
+23        //O(col * row)
+24        for(int j = 0; j < m; j++) {
+25            int spaceBottomRow = n-1;
+26            for(int i = n-1; i >= 0; i--) {
+27                if(result[i][j] == '*') {
+28                    spaceBottomRow = i-1;
+29                    continue;
+30                }
 31
-32        return ans;
-33    }
-34};
+32                if(result[i][j] == '#') {
+33                    result[i][j] = '.';
+34                    result[spaceBottomRow][j] = '#';
+35                    spaceBottomRow--;
+36                }
+37            }
+38        }
+39
+40        return result;
+41    }
+42};
+43
