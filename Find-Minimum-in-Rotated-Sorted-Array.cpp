cc1bb449912a38419782be02e8b07f5d3bb0bdd4
Aman Tripathi <144323288+tripathiaman0709@users.noreply.github.com>
2026-05-15 17:13:21 +0530

    [153] [Time Beats: 100.00%] [Memory Beats: 48.18%] - LeetPush

--- Find-Minimum-in-Rotated-Sorted-Array.cpp
+++ Find-Minimum-in-Rotated-Sorted-Array.cpp
@@ -1,25 +1,22 @@
 1class Solution {
 2public:
-3
-4    int findMin(vector<int>& v) {
-5
-6        int low = 0;
-7        int high = v.size() - 1;
-8
-9        while(low < high){
-10
-11            int mid = low + (high - low) / 2;
-12
-13            if(v[mid] > v[high]){
-14
-15                low = mid + 1;
-16
-17            }else{
-18
-19                high = mid;
-20            }
-21        }
-22
-23        return v[low];
-24    }
-25};
+3    int findMin(vector<int>& a) {
+4        int n=a.size();
+5        int low=0;
+6        int high=n-1;
+7        while(low<high){
+8            if(a[low]<a[high]){
+9                return a[low];
+10            }else{
+11                int mid=low+(high-low)/2;
+12                if(a[mid]>=a[low]){
+13                    low=mid+1;
+14                }else{
+15                    high=mid;
+16                }
+17            }
+18        }
+19        return a[low];
+20
+21    }
+22};
