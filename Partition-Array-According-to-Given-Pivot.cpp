bcf1bc0f214b8e2ec8e20fc4b4e6f887b476726e
Aman Tripathi <tripathiaman0709@gmail.com>
2026-06-08 13:34:02 +0530

    [2161] [Time Beats: 11.39%] [Memory Beats: 7.33%] - LeetPush

--- Partition-Array-According-to-Given-Pivot.cpp
+++ Partition-Array-According-to-Given-Pivot.cpp
@@ -1,33 +1,34 @@
-class Solution {
-public:
-    vector<int> pivotArray(vector<int>& v, int p) {
-        //solving it in a simple way
-        //without using the pivot element technique
-
-        vector<int>small;
-        vector<int>large;
-        vector<int>equal;
-        vector<int>ans;
-        
-        int n=v.size();
-        for(int i=0;i<n;i++){
-            if(v[i]<p){
-                small.push_back(v[i]);
-            }else if(v[i]>p){
-                large.push_back(v[i]);
-            }else{
-                equal.push_back(v[i]);
-            }
-        }
-        for(auto it:small){
-            ans.push_back(it);
-        }
-        for(auto it:equal){
-            ans.push_back(it);
-        }
-        for(auto it:large){
-            ans.push_back(it);
-        }
-        return ans;
-    }
-};
+1class Solution {
+2public:
+3    vector<int> pivotArray(vector<int>& v, int k) {
+4        vector<int>less;
+5        vector<int>equal;
+6        vector<int>more;
+7
+8        int n=v.size();
+9
+10        for(int i=0;i<n;i++){
+11            if(v[i]==k){
+12                equal.push_back(v[i]);
+13            }else if(v[i]<k){
+14                less.push_back(v[i]);
+15            }else{
+16                more.push_back(v[i]);
+17            }
+18        }
+19
+20        vector<int>ans;
+21
+22        for(auto it:less){
+23            ans.push_back(it);
+24        }
+25        for(auto it:equal){
+26            ans.push_back(it);
+27        }
+28        for(auto it:more){
+29            ans.push_back(it);
+30        }
+31
+32        return ans;
+33    }
+34};
