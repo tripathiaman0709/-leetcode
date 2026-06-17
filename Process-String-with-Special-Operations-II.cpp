5a7b82e4f6261917285383a375b8c346aa819555
Aman Tripathi <tripathiaman0709@gmail.com>
2026-06-17 23:49:18 +0530

    [3614] [Time Beats: 77.01%] [Memory Beats: 51.72%] - LeetPush

--- Process-String-with-Special-Operations-II.cpp
+++ Process-String-with-Special-Operations-II.cpp
@@ -1,30 +1,30 @@
-typedef long long ll;
-class Solution {
-public:
-    char processStr(string s, long long k) {
-        ll len = 0;
-        for(auto c:s){
-            if(islower(c)) len++;
-            else if(c=='*' && len>0) len--;
-            else if(c=='#') len *= 2;
-        }
-        if(k >= len) return '.';
-
-        for(int i = s.size()-1; i >= 0; --i){
-            char c = s[i];
-            if(islower(c)) {
-                if(k == len - 1) return c;
-                len--;
-            }
-            else if(c == '*') len++;
-            else if(c == '#') {
-                len /= 2;
-                if(k >= len) k -= len;
-            }
-            else if(c == '%') {
-                k = len - 1 - k;
-            }
-        }
-        return '.';
-    }
-};
+1typedef long long ll;
+2class Solution {
+3public:
+4    char processStr(string s, long long k) {
+5        ll len = 0;
+6        for(auto c:s){
+7            if(islower(c)) len++;
+8            else if(c=='*' && len>0) len--;
+9            else if(c=='#') len *= 2;
+10        }
+11        if(k >= len) return '.';
+12
+13        for(int i = s.size()-1; i >= 0; --i){
+14            char c = s[i];
+15            if(islower(c)) {
+16                if(k == len - 1) return c;
+17                len--;
+18            }
+19            else if(c == '*') len++;
+20            else if(c == '#') {
+21                len /= 2;
+22                if(k >= len) k -= len;
+23            }
+24            else if(c == '%') {
+25                k = len - 1 - k;
+26            }
+27        }
+28        return '.';
+29    }
+30};
