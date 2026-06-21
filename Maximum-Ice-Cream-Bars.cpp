ac50323136eedd7364c77948b6a387ce2c36d711
Aman Tripathi <tripathiaman0709@gmail.com>
2026-06-21 16:02:59 +0530

    [1833] [Time Beats: 84.16%] [Memory Beats: 25.28%] - LeetPush

--- Maximum-Ice-Cream-Bars.cpp
+++ Maximum-Ice-Cream-Bars.cpp
@@ -1,15 +1,29 @@
 1class Solution {
 2public:
 3    int maxIceCream(vector<int>& v, int k) {
-4        int n=v.size();
-5        sort(v.begin(),v.end());
-6        int ans=0;
-7        for(auto it:v){
-8            k-=it;
-9            if(k>=0)ans++;
-10            else break;
+4        if (v.empty()) return 0;
+5    
+6        int max_val = *max_element(v.begin(), v.end());
+7        vector<int> count(max_val + 1, 0);
+8        
+9        for (int it : v) {
+10            count[it]++;
 11        }
-12
-13        return ans;
-14    }
-15};
+12        
+13        int ans = 0;
+14        
+15        for (int cost = 1; cost <= max_val; ++cost) {
+16            if (count[cost] > 0) {
+17                int take = min(count[cost], k / cost);
+18                ans += take;
+19                k -= take * cost;
+20                
+21                if (k < cost) {
+22                    break;
+23                }
+24            }
+25        }
+26        
+27        return ans;
+28    }
+29};
