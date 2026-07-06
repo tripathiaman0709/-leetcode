b6f34b7ad3d6cdda88b2160f5471669a20dfbdfd
Aman Tripathi <tripathiaman0709@gmail.com>
2026-07-06 18:57:38 +0530

    [1288] [Time Beats: 100.00%] [Memory Beats: 60.13%] - LeetPush

--- 1288-Remove-Covered-Intervals.cpp
+++ 1288-Remove-Covered-Intervals.cpp
@@ -1,37 +1,23 @@
 class Solution {
 public:
-    int removeCoveredIntervals(vector<vector<int>>& v) {
-        //can use a double for loop 
-        int n=v.size();
-        for(int i=0;i<n;i++){
-            int start=v[i][0];
-            int end=v[i][1];
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](const auto& a, const auto& b) {
+            if (a[0] == b[0]) {
+                return a[1] > b[1];
+            }
+            return a[0] < b[0];
+        });
 
-            for(int j=0;j<n;j++){
-                int curstart=v[j][0];
-                int curend=v[j][1];
+        int ans = 0;
+        int maxEnd = -1;
 
-                if(j==i)continue;
-
-                if(curstart>=start && curend<=end){
-                    v[j][0]=-1;
-                    v[j][1]=-1;
-                }
-
+        for (const auto& interval : intervals) {
+            if (interval[1] > maxEnd) {
+                ans++;
+                maxEnd = interval[1];
             }
         }
 
-        int count=0;
-
-        for(int j=0;j<n;j++){
-                int curstart=v[j][0];
-                int curend=v[j][1];
-
-                if(curstart==-1){
-                    count++;
-                }
-        }
-
-        return n-count;
+        return ans;
     }
 };
