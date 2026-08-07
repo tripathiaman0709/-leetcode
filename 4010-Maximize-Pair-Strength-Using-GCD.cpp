f617b6f604afbab05a32bbb2cd6ebe3069caf129
Aman Tripathi <tripathiaman0709@gmail.com>
2026-08-07 20:41:43 +0530

    [4010] [Time Beats: 29.99%] [Memory Beats: 6.56%] - LeetPush

---
+++ 4010-Maximize-Pair-Strength-Using-GCD.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long maxPairStrength(vector<int>& v) {
+        long long n=v.size();
+        long long maxi=0;
+        for(long long i=0;i<n;i++){
+            long long x=v[i];
+            for(long long j=i+1;j<n;j++){
+                long long y=v[j];
+                long long hcf=__gcd(x,y);
+                hcf*=hcf;
+                long long pro=x*y;
+
+                long long cal=pro/hcf;
+                maxi=max(maxi,cal);
+            }
+        }
+
+        return maxi;
+    }
+};
